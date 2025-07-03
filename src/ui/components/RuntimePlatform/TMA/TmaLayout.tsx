53eaa5d8799d96b1d929f62fadd2a2c78c0838d5
Sven <svenlai@Sven-For-MacBook-Pro.local>
2025-07-03 15:34:47 +0800

    fix: storage key

--- src/ui/components/RuntimePlatform/TMA/TmaLayout.tsx
+++ src/ui/components/RuntimePlatform/TMA/TmaLayout.tsx
@@ -59,7 +59,7 @@ function Loader({ children }: PropsWithChildren) {
 		const startParam = retrieveLaunchParams()?.startParam;
 		console.log("startParam", startParam);
 
-		sessionStorage.setItem("temp-startParam", JSON.stringify(startParam));
+		sessionStorage.setItem(ogParamsKeys, JSON.stringify(startParam));
 
 		if (!startParam) return;
 
